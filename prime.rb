c53f779b8ea768dd71bce4011ff78dd674bd6c76
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-07 18:43:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(num)
+
+#prime number dose not accept division except by 1 and  itself .
+   if num < 2
+     false
+   else
+     i=2
+     while i < num
+       if num % i == 0
+         return false
+       end
+        i += 1
+     end
+      return true
+   end
+end
